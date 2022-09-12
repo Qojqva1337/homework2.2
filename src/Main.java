9e326905963b9f890b5a10efb4744122d618fd5a
artur <artursuper10@gmail.com>
2022-09-12 12:59:18 +0300

    Задание

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+    public static void main(String[] args) {
+       // Задача 1
+        System.out.println("Задача 1");
+        int man = 19;
+        if (man >= 18) {
+            System.out.println("Поздравляю с совершенолетие");
+        }
+        if (man < 18) {
+            System.out.println("возраст совершеннолетия ещё не наступил, и нужно немного подождать");
+        }
+
+        // Задача 2
+        System.out.println("Задачас 2");
+        int child = 8;
+        if (child >= 7) {
+            System.out.println("Ребенок ходит в школу");
+        }
+
+
+    }
+}
